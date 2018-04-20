41be19a48d53a263ffc0bb9b6b2addc2ef4c7b7c
jaredfitton <jaredfitton@gmail.com>
2018-04-20 09:56:12 -0700

    Base Files

---
+++ env.sh
@@ -0,0 +1,8 @@
+export MONGO_USERNAME=dbuser1
+export MONGO_PASSWORD=jaredjeremymichael
+export MONGO_HOST=ds111535.mlab.com
+export MONGO_PORT=11535
+export MONGO_DBNAME=forumapp2
+export SECRET_KEY='\x80`\xcd,9\x98\x0b\x902m\xb5\xb9\xae^\x9c\xa4\\\xfb\xb8e\xc8\x7f\xae{'
+export GITHUB_CLIENT_ID=e9b3dace683b0da722a5
+export GITHUB_CLIENT_SECRET=8a25ce17db2359583678316905c251007291cd1d
---
+++ webapp.py
@@ -0,0 +1,138 @@
+from flask import Flask, redirect, url_for, session, request, jsonify, Markup
+from flask_oauthlib.client import OAuth
+from flask import render_template
+from bson.objectid import ObjectId
+
+import pymongo
+import pprint
+import os
+import json
+
+os.system("echo '[]'>" + 'forum.json')
+
+app = Flask(__name__)
+
+app.debug = True #Change this to False for production
+
+app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
+oauth = OAuth(app)
+
+url = 'mongodb://{}:{}@{}:{}/{}'.format(
+    os.environ["MONGO_USERNAME"],
+    os.environ["MONGO_PASSWORD"],
+    os.environ["MONGO_HOST"],
+    os.environ["MONGO_PORT"],
+    os.environ["MONGO_DBNAME"])
+
+client = pymongo.MongoClient(url)
+db = client[os.environ["MONGO_DBNAME"]]
+collection = db["posts"]
+
+#Set up GitHub as OAuth provider
+github = oauth.remote_app(
+    'github',
+    consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your dfweb app's "password" for github's OAuth
+    request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+)
+
+#use a JSON file to store the past posts.  A global list variable doesn't work when handling multiple requests coming in and being handled on different threads
+#Create and set a global variable for the name of you JSON file here.  The file will be created on Heroku, so you don't need to make it in GitHub
+
+
+@app.context_processor
+def inject_logged_in():
+    return {"logged_in":('github_token' in session)}
+
+@app.route('/')
+def home():
+    return render_template('home.html', past_posts=posts_to_html())
+
+def posts_to_html():
+    forum_table = Markup("<table class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
+    for post in collection.find():
+        try:
+            # print(post["_id"])
+            # print("got into for collection.find")
+            # {'_id': ObjectId('5ab3df4f0bac5800099e1fd4')
+
+            docid=str(post["_id"])
+
+            print(docid)
+            # print(docid)
+            forum_table += Markup("<tr> <td>" + post["username"] + "</td> <td>" + post["message"])
+            if session['user_data']['login'] == post["username"]: #we session user is the same as poster
+                forum_table += Markup('<form action = "/delete" method = "post"> <button value=' + docid + ' type="submit" name="delete" class="btn btn-secondary">Delete</button> </form>')
+                 #adds another column to the table with a delete button this is the code jared needs
+            forum_table += Markup("</td> </tr>")
+        except Exception as e:
+            print(e)
+    forum_table += Markup("</table>")
+    return forum_table
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    print("In delete method" + request.form["delete"])
+
+    docid = request.form["delete"]
+
+    collection.delete_one({'_id': ObjectId(docid)})
+
+    return render_template('home.html', past_posts = posts_to_html())
+
+
+@app.route('/posted', methods=['POST'])
+def post():
+    username_local = session['user_data']['login']
+    message_local = request.form['message']
+    try:
+        collection.insert( { "username": username_local, "message": message_local } )
+    except Exception as e:
+        print("Unable to post :(")
+        print(e)
+
+    return render_template('home.html', past_posts = posts_to_html())
+
+    #This function should add the new post to the JSON file of posts and then render home.html and display the posts.
+    #Every post should include the username of the poster and text of the post.
+
+#redirect to GitHub's OAuth page and confirm callback URL
+@app.route('/login')
+def login():
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('message.html', message='You were logged out')
+
+@app.route('/login/authorized')
+def authorized():
+    resp = github.authorized_response()
+    if resp is None:
+        session.clear()
+        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)
+    else:
+        try:
+            session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
+            session['user_data']=github.get('user').data
+            message='You were successfully logged in as ' + session['user_data']['login']
+        except Exception as inst:
+            session.clear()
+            print(inst)
+            message='Unable to login, please try again.  '
+    return render_template('message.html', message=message)
+
+#the tokengetter is automatically called to check who is logged in.
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
+
+
+if __name__ == '__main__':
+    app.run()

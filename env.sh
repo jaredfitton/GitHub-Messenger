56108ced14118b5da408adb1fc4304b74d9977f5
Michael Moschitto <michaelmoschitto@gmail.com>
2018-04-23 09:22:06 -0700

    nochange

--- env.sh
+++ env.sh
@@ -5,5 +5,5 @@
 # export MONGO_DBNAME=forumapp2
 
 export SECRET_KEY='\x80`\xcd,9\x98\x0b\x902m\xb5\xb9\xae^\x9c\xa4\\\xfb\xb8e\xc8\x7f\xae{'
-export GITHUB_CLIENT_ID=c4e78272779b34665b47
-export GITHUB_CLIENT_SECRET=b2332459c009b11f2d64b734c5044a7356bee8fe
+export GITHUB_CLIENT_ID=c4e78272779b34665b47  #updated
+export GITHUB_CLIENT_SECRET=b2332459c009b11f2d64b734c5044a7356bee8fe #updated

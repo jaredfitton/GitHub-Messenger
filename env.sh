e01c6abb007e0178ba3e507d14a471c256878e39
JeremyKnight <jeremyknightjck99@gmail.com>
2018-04-23 09:16:08 -0700

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 # export MONGO_USERNAME=dbuser1
 # export MONGO_PASSWORD=jaredjeremymichael
-# export MONGO_HOST=ds111535.mlab.com
-# export MONGO_PORT=11535
-# export MONGO_DBNAME=forumapp2
+# export MONGO_HOST=ds131151.mlab.com
+# export MONGO_PORT=31151
+# export MONGO_DBNAME=github-oauth
 
 export SECRET_KEY='\x80`\xcd,9\x98\x0b\x902m\xb5\xb9\xae^\x9c\xa4\\\xfb\xb8e\xc8\x7f\xae{'
 export GITHUB_CLIENT_ID=c4e78272779b34665b47

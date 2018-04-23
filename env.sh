b65249579e7c6f1bbbf0dbacfabdcea2eae8c286
jaredfitton <jaredfitton@gmail.com>
2018-04-23 09:12:36 -0700

    Added GitHub Clients Ids

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
-export MONGO_USERNAME=dbuser1
-export MONGO_PASSWORD=jaredjeremymichael
-export MONGO_HOST=ds111535.mlab.com
-export MONGO_PORT=11535
-export MONGO_DBNAME=forumapp2
+# export MONGO_USERNAME=dbuser1
+# export MONGO_PASSWORD=jaredjeremymichael
+# export MONGO_HOST=ds111535.mlab.com
+# export MONGO_PORT=11535
+# export MONGO_DBNAME=forumapp2
+
 export SECRET_KEY='\x80`\xcd,9\x98\x0b\x902m\xb5\xb9\xae^\x9c\xa4\\\xfb\xb8e\xc8\x7f\xae{'
-export GITHUB_CLIENT_ID=e9b3dace683b0da722a5
-export GITHUB_CLIENT_SECRET=8a25ce17db2359583678316905c251007291cd1d
+export GITHUB_CLIENT_ID=c4e78272779b34665b47
+export GITHUB_CLIENT_SECRET=b2332459c009b11f2d64b734c5044a7356bee8fe

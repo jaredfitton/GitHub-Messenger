01a458976cf776336bc37edc926333d97c25cc19
jaredfitton <jaredfitton@gmail.com>
2018-05-01 09:40:00 -0700

    Error checking for get_user_location

--- webapp.py
+++ webapp.py
@@ -130,8 +130,8 @@ def authorized():
             print(inst)
             flash('unable to login')
             # message='Unable to login, please try again.  '
-
-    return render_template('home.html', past_posts = posts_to_html(get_user_location()))
+    print(get_user_location())
+    return render_template('home.html', past_posts = posts_to_html("SB"))
 
 #the tokengetter is automatically called to check who is logged in.
 @github.tokengetter

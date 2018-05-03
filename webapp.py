13e939995952b732961eae3fbc98dec7b03fcf4b
jeremyknight <jeremyknightjck99@gmail.com>
2018-05-03 09:14:10 -0700

    added to posts_to_html

--- webapp.py
+++ webapp.py
@@ -63,6 +63,8 @@ def home():
 
 def posts_to_html(user_location):
     print("User's location: " + user_location)
+    if user_location == "no location":
+        return Markup("<p> Set location in your github bio  to find people! </p>")
     forum_table = Markup("<table class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
     for post in collection.find({"location": user_location}):
         try:

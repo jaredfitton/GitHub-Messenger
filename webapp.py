c127976f4b79bcb2a8cb8c609deb0ddf3e3bf45e
Michael Moschitto <michaelmoschitto@gmail.com>
2018-05-11 09:25:47 -0700

    scroll

--- webapp.py
+++ webapp.py
@@ -64,7 +64,7 @@ def posts_to_html(user_location):
     # if user_location == "no location":
     #     flash('Set location in your github bio to find people!')
     #     return ""
-    forum_table = Markup("<table class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
+    forum_table = Markup("<table id='messageTable' class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
     for post in collection.find({"location": user_location}):
         try:
             forum_table += Markup("<tr> <td>" + post["username"] + "</td> <td>" + post["message"] + "</td> </tr>")

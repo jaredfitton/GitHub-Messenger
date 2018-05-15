3500a5e426146eb55044fa06e18526e821b1af45
jeremyknight <jeremyknightjck99@gmail.com>
2018-05-15 09:49:41 -0700

    moved no location conditional up in posts_to_html

--- webapp.py
+++ webapp.py
@@ -60,14 +60,10 @@ def home():
         return render_template('home.html')
 
 def posts_to_html(user_location):
-    #print("User's location: " + user_location)
-    #if user_location == "no location":
-    #    flash('no location set')
-    #    return ""
-    forum_table = Markup("<table class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
     if user_location == "no location":
          flash('no location set')
          return ""
+    forum_table = Markup("<table class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
     forum_table = Markup("<table id='messageTable' class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
     for post in collection.find({"location": user_location}):
         try:

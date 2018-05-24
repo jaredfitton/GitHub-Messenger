d061f5a17086738e2ad7581032b092be0a5e8d46
jeremyknight <jeremyknightjck99@gmail.com>
2018-05-24 11:00:38 -0700

    added location stuff in context processor

--- webapp.py
+++ webapp.py
@@ -49,7 +49,7 @@ github = oauth.remote_app(
 @app.context_processor
 def inject_logged_in():
     # print("logged in")
-    if location = session['user_data']['location']
+    location = session['user_data']['location']
     if isinstance(location, str):
         return {"logged_in":('github_token' in session), "location_set":(True)}
     return {"logged_in":('github_token' in session), "location_set":(False)}

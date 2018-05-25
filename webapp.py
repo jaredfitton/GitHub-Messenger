bd1b82f94a2dab5987ade9c6c2a043ad1cbf8807
Michael Moschitto <michaelmoschitto@gmail.com>
2018-05-25 09:26:55 -0700

    reverse

--- webapp.py
+++ webapp.py
@@ -72,7 +72,7 @@ def posts_to_html(user_location):
     forum_table = Markup("<table id='messageTable' class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
     for post in collection.find({"location": user_location}):
         try:
-            forum_table = Markup("<tr> <td>" + post["username"] + "</td> <td>" + post["message"] + "</td> </tr>") + forum_table
+            forum_table = Markup("<table id='messageTable' class='table table-bordered'>" "<tr> <td>" + post["username"] + "</td> <td>" + post["message"] + "</td> </tr>") + forum_table
         except Exception as e:
             print(e)
     forum_table += Markup("</table>")

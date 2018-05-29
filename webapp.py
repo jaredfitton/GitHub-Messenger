ac0c57516909c9c1e3fb9b72d886d5bd5609fddc
jeremyknight <jeremyknightjck99@gmail.com>
2018-05-29 09:19:20 -0700

    logout button migt show now

--- webapp.py
+++ webapp.py
@@ -63,20 +63,17 @@ def home():
         return render_template('home.html')
 
 def posts_to_html(user_location):
-    forum_table=""
-    # forum_table = Markup("<tr><td></td></tr>")
     if user_location == "no location":
         flash('no location set')
         print('no location set')
         return ""
-    # forum_table = Markup("<table class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
-    try:
-        for post in collection.find({"location": user_location}):
-            forum_table = Markup( "<tr> <td>" + post["username"] + "</td> <td>" + post["message"] + "</td> </tr>") + forum_table
-        forum_table = Markup("<table id='messageTable' class='table table-bordered'> <tr> <th id='tablehead'> Username </th> <th> Message </th> </tr>") + forum_table
-
-    except Exception as e:
-        print(e)
+    forum_table = Markup("<table class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
+    forum_table = Markup("<table id='messageTable' class='table table-bordered'> <tr> <th> Username </th> <th> Message </th> </tr>")
+    for post in collection.find({"location": user_location}):
+        try:
+            forum_table += Markup("<tr> <td>" + post["username"] + "</td> <td>" + post["message"] + "</td> </tr>")
+        except Exception as e:
+            print(e)
     forum_table += Markup("</table>")
     return forum_table
 
@@ -132,7 +129,7 @@ def authorized():
         try:
             session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
             session['user_data']=github.get('user').data
-            #session['location']=get_user_location()
+            session['location']=get_user_location()
             flash('You were successfully logged in as ' + session['user_data']['login'])
             flash('logged in')
             # message='You were successfully logged in as ' + session['user_data']['login']

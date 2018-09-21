11c3de0931c91cb30128cfdf3c211dd395dafa76
Jared Fitton <jaredfitton@gmail.com>
2018-09-20 19:23:58 -0700

    Delete env.sh

--- env.sh
+++
@@ -1,8 +0,0 @@
-export MONGO_USERNAME=dbuser1
-export MONGO_PASSWORD=jaredjeremymichael
-export MONGO_HOST=ds159129.mlab.com
-export MONGO_PORT=59129
-export MONGO_DBNAME=github-messenger
-export SECRET_KEY='\x80`\xcd,9\x98\x0b\x902m\xb5\xb9\xae^\x9c\xa4\\\xfb\xb8e\xc8\x7f\xae{'
-export GITHUB_CLIENT_ID=c4e78272779b34665b47
-export GITHUB_CLIENT_SECRET=d78da7067d5831e3fec01b330326c2b5bfcc30e7
